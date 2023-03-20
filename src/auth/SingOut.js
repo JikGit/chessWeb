7c37d7d5abe6c110d5852e611b1cc396ce2c64db
JikGit <dnlbrll@gmail.com>
2023-03-20 17:44:49 +0100

    modifiche

--- src/auth/SingOut.js
+++ src/auth/SingOut.js
@@ -2,7 +2,7 @@ import '../css/singOut.css'
 function SingOut() {
 	function exitAcc(){
 		localStorage.setItem("userName", "")
-		window.location.href='/';
+		window.location.href = String(window.location).split("/matches")[0];
 	}
 
 	return (
